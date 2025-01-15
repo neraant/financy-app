6f60fde9cd2302f4b71a308b5a2928f281326457
neraant <cherednik0311@gmail.com>
2025-01-15 23:53:55 +0300

    changing register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import Statistic from './components/Statistic'
 import UserActivesList from './components/UserActivesList'
-import UserBalanceInfo from './components/userBalanceInfo'
+import UserBalanceInfo from './components/UserBalanceInfo'
 
 const App = () => {
   return (
